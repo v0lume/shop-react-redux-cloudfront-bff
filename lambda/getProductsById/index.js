e2a3b76b756d3f48382bec3147248355dc2a9549
ashmanouski-acertus <artisom.shmanouski@acertusdelivers.com>
2023-03-06 00:49:43 +0300

    task-4

--- lambda/getProductsById/index.js
+++ lambda/getProductsById/index.js
@@ -1,9 +1,17 @@
-const products = require('../../data/products.json');
+const { DynamoDB } = require('aws-sdk');
+
+const db = new DynamoDB.DocumentClient();
 
 module.exports.handler = async (event) => {
+    console.log(event);
+
     const { pathParameters: { id } = {} } = event;
 
+    const { Items: products = [] } = await db.scan({ TableName: 'products' }).promise();
+    const { Items: stocks = [] } = await db.scan({ TableName: 'stocks' }).promise();
+
     const product = products.find((item) => item.id === id);
+    const stock = stocks.find((item) => item.product_id === id);
 
     if (!product) {
         return {
@@ -14,7 +22,7 @@ module.exports.handler = async (event) => {
 
     return {
       statusCode: 200,
-      body: JSON.stringify(product),
+      body: JSON.stringify({ ...product, count: stock?.count || 0 }),
     };
   };
   
--- lambda/getProductsList/index.js
+++ lambda/getProductsList/index.js
@@ -1,14 +1,23 @@
-const products = require('../../data/products.json');
+const { DynamoDB } = require('aws-sdk');
+
+const db = new DynamoDB.DocumentClient();
+
+// const TableNameProducts = process.env.TABLE_PRODUCTS;
+// const TableNameStocks = process.env.TABLE_STOCKS;
 
 module.exports.handler = async (event) => {
+    console.log(event);
+
+    const { Items: products = [] } = await db.scan({ TableName: 'products' }).promise();
+    const { Items: stocks = [] } = await db.scan({ TableName: 'stocks' }).promise();
+
     return {
       statusCode: 200,
-      body: JSON.stringify(products.map((item) => ({
-        id: item.id,
-        title: `${item.manufacturer} ${item.model} ${item.package}`,
-        description: `${item.fuel}, ${item.transmission} transmision, ${item.power}hp`,
-        price: `${item.price} ${item.currency}`,
-      }))),
+      body: JSON.stringify(products.map((item) => {
+        const stock = stocks.find((stock) => stock.product_id === item.id);
+
+        return { ...item, count: stock?.count || 0 };
+      })),
     };
   };
   
